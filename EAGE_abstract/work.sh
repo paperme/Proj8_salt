40155a122339d17ada9b84045f4a8e317118bf56
wenlong <simbawwl@gmail.com>
2018-01-11 09:15:59 +0800

    first commit

---
+++ EAGE_abstract/work.sh
@@ -0,0 +1,9 @@
+latex    work.tex
+latex    work.tex
+dvips    work
+ps2pdf   work.ps
+pdffonts work.pdf
+#evince   work.pdf &
+rm *.aux *.bbl *.blg *.dvi *.lof *.log *.ps *.nav *.out *.snm *.toc
+
+
---
+++ README.md
@@ -0,0 +1,4 @@
+Draft paper about using ML to detect salt
+
+Wenlong
+Sep 14, 2017
